d4d75f0dc3f75bd4e6970509945c577e99844c81
aaronsisler <asisler@corp.epnet.com>
2018-01-26 08:47:58 -0500

    Changing structure of position watchers to handle the notifications change

--- src/app/position/services/position-watcher.service.ts
+++ src/app/position/services/position-watcher.service.ts
@@ -10,11 +10,11 @@ export class PositionWatcherService {
   constructor(private db: AngularFireDatabase) { }
 
   getAll(userId: string): Observable<PositionWatcher> {
-    return this.db.object('/position-watchers/' + userId).map(x => new PositionWatcher(x.positions));
+    return this.db.object('/position-watchers/' + userId).map(x => new PositionWatcher(x));
   }
 
   getPosition(userId: string, key: string): FirebaseObjectObservable<PositionWatched> {
-    return this.db.object('position-watchers/' + userId + '/positions/' + key);
+    return this.db.object('position-watchers/' + userId + '/' + key);
   }
 
   updateSubscriptionLevel(userId: string, key: string, subscriptionLevel: string) {
