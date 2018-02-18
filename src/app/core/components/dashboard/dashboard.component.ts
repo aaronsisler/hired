0d440d969db1b9013668a5ab9418251442a3c481
aaronsisler <asisler@corp.epnet.com>
2018-02-18 16:01:31 -0500

    Hiding positions tab for isEmployee status now working

--- src/app/core/components/dashboard/dashboard.component.ts
+++ src/app/core/components/dashboard/dashboard.component.ts
@@ -1,3 +1,5 @@
+import { AppUser } from './../../../shared/models/app-user';
+import { UserService } from 'shared/services/user.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 import { AuthService } from 'shared/services/auth.service';
@@ -9,12 +11,16 @@ import { AuthService } from 'shared/services/auth.service';
 })
 export class DashboardComponent implements OnInit, OnDestroy {
   userSubscription: Subscription;
-  userId: string;
+  user: AppUser;
+  //userId: string;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private userService: UserService) { }
 
   async ngOnInit() {
-    this.userSubscription = await this.authService.user$.subscribe(user => this.userId = user.uid);
+    this.userSubscription = await this.authService.user$
+      .switchMap(authUser => this.userService.get(authUser.uid))
+      .subscribe(user => this.user = user);
+    //subscribe( user => this.userId = user.uid);
   }
 
   ngOnDestroy() {
